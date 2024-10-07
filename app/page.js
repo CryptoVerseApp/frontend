5001c4bacbaffc2c75daac156758b683f76a4435
alikamatu <alikamaatu14@gmail.com>
2024-10-07 03:27:59 +0000

    update

--- app/page.js
+++ app/page.js
@@ -4,6 +4,10 @@ export default function Home() {
 
   return (
     <div className="">
+      <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       <HomaPage />
     </div>
   );
